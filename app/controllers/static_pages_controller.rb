0c1d37d6a73d9c472f1a9e4d8dd4b42b5e134ad7
Mark Miyashita <negativetwelve@gmail.com>
2012-11-18 07:12:35 -0800

    convert to string

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     @songs = []
     if signed_in?
-      current_user.playlists[current_user.current_playlist_id].songs.each do |song|
+      current_user.playlists[current_user.current_playlist_id.to_i].songs.each do |song|
         @songs += [{title: song.title, mp3: song.stream_url.to_s + "?client_id=7369d5fe3bc71c4163f76b36b04b1128", artwork_url: song.artwork_url}]
       end
     else
