eb199e21df67a0aeb0cb362fe9c6dbca2a62aac2
Mark Miyashita <negativetwelve@gmail.com>
2012-11-18 01:51:17 -0800

    cover art booyah

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,7 @@ class StaticPagesController < ApplicationController
       @songs = []
       Song.all.each do |song|
         if song.streamable?
-          @songs += [{title: song.title, mp3: song.stream_url.to_s + "?client_id=7369d5fe3bc71c4163f76b36b04b1128"}]
+          @songs += [{title: song.title, mp3: song.stream_url.to_s + "?client_id=7369d5fe3bc71c4163f76b36b04b1128", artwork_url: song.artwork_url}]
         end
       end
     end
