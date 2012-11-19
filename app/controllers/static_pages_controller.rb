9e6587a52352ce2930a39b627dba29274a2176e3
Mark Miyashita <negativetwelve@gmail.com>
2012-11-19 14:01:25 -0800

    playlist songs have to be streamable

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,9 @@ class StaticPagesController < ApplicationController
     @songs = []
     if signed_in?
       current_user.playlists[current_user.current_playlist_id.to_i].songs.each do |song|
-        @songs += [{title: song.title, mp3: song.stream_url.to_s + "?client_id=7369d5fe3bc71c4163f76b36b04b1128", artwork_url: song.artwork_url}]
+        if song.streamable?
+          @songs += [{title: song.title, mp3: song.stream_url.to_s + "?client_id=7369d5fe3bc71c4163f76b36b04b1128", artwork_url: song.artwork_url}]
+        end
       end
     else
       (0..20).each do |num|
